8922df8bff34f7e54d0fb6bf0ef3d9fe459360a3
marianoperri <marianoperri@gmail.com>
2020-11-02 11:10:50 -0300

    guardando los cambios de la eda02 en mi rama para subir

---
+++ js/contacto.js
@@ -0,0 +1,48 @@
+var regexCampoNumerico=/^[0-9]+$/
+var regexCampoTexto=/^[a-z][A-Z]+$/
+var regexTelefono=/^[0-9]{4}-[0-9]{4}$/
+var regexEmail=/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/
+
+function validar(){
+    var error=false;
+    var mensajesError="";
+
+    if (document.getElementById("nombre").value ==='') {
+		error= true
+		mensajeError+="*Ingrese su nombre "
+	}
+    if(!regexTelefono.test(document.getElementById("numero").value)){
+        error=true;
+        mensajesError+="Tiene que ingresar un telefono";
+    }
+    if(!regexEmail.test(document.getElementById("mail").value)){
+        error=true;
+        mensajesError+="Tiene que ser un email valido";
+    }
+
+    if(error){
+        /*mostrar los mensajes de error*/
+        document.getElementById("mensaje").innerHTML=mensajesError;
+        return false;
+    }else{
+        return false;
+    }
+}
+
+
+
+const CANTIDAD =1000;
+
+function contarCaracteres(){
+	var caracteresEscritos= document.getElementById('consulta').value.length;
+	var restantes= CANTIDAD-caracteresEscritos;
+	if (restantes<500) {
+		document.getElementById("caracteres").style.color="red";
+	} else {
+		document.getElementById("caracteres").style.color="black";
+	}
+
+	document.getElementById("caracteres").innerHTML=restantes;
+}
+
+
