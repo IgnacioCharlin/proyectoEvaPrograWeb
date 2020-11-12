dd7974a4608fba9e9458371b7c964a568c1212be
Ignacio Charlin <ignaciocharlin1995@gmail.com>
2020-11-12 08:58:07 -0300

    realizando punto 1 del slider y poniendo menu desplegable

---
+++ jss/funciones.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){ 
+    $(".desplegable").click(function(){
+      if($('.barra-inferior-nav').is(":visible")){
+          $('.barra-inferior-nav').slideUp();
+      }else{
+        $('.barra-inferior-nav').slideDown();
+      }
+    });
+    $(window).resize(function(){
+        if($(this).width()>768){
+            $(".barra-inferior-nav").show();
+        } else{
+            $(".barra-inferior-nav").hide();
+        }
+    });
+});
